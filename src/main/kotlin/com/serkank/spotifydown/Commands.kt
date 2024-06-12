e1db8e51b0b78bdce426926ba4fe541704c978a1
Serkan Kaba <serkan_kaba@yahoo.com>
2024-06-12 07:00:39 +0200

    Clean

--- src/main/kotlin/com/serkank/spotifydown/Commands.kt
+++ src/main/kotlin/com/serkank/spotifydown/Commands.kt
@@ -9,22 +9,19 @@ import org.springframework.web.client.RestClient
 private val logger = KotlinLogging.logger {}
 
 @Command
-class Commands {
-
-    @Autowired
-    lateinit var restClientBuilder: RestClient.Builder
+class Commands(private val restClientBuilder: RestClient.Builder) {
 
     @Command
     fun download(@Pattern(regexp = ALL_URL_PATTERN, message = "Not a valid Spotify URL") url: String) {
         logger.info { "Downloading $url" }
         val matchResult = ALL_URL_PATTERN.toRegex().find(url)
         val type = enumValueOf<Type>(matchResult?.groupValues?.get(1).toString().uppercase())
-        val id = matchResult?.groupValues?.get(2)
+        val id = matchResult?.groupValues?.get(2)!!
 
         when (type) {
-            Type.TRACK -> Track(id!!, restClientBuilder).download()
+            Type.TRACK -> Track(id, restClientBuilder).download()
             Type.ALBUM -> TODO()
-            Type.PLAYLIST -> Playlist(id!!, restClientBuilder).download()
+            Type.PLAYLIST -> Playlist(id, restClientBuilder).download()
         }
     }
 
