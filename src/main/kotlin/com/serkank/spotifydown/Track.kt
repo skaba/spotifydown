55eb9e8d3abc243037edacb82cde158a29577414
Serkan Kaba <serkan_kaba@yahoo.com>
2024-06-12 09:14:13 +0200

    Clean

--- src/main/kotlin/com/serkank/spotifydown/Track.kt
+++ src/main/kotlin/com/serkank/spotifydown/Track.kt
@@ -9,35 +9,6 @@ private val logger = KotlinLogging.logger {}
 
 class Track(id: String, restClientBuilder: RestClient.Builder) : Downloadable(id, restClientBuilder) {
 
-    /*init {
-        val response = request("https://api.spotifydown.com/download/$id")
-
-            .execute()
-            .returnContent()
-            .asString()
-        val downloadResponse = Json {
-            ignoreUnknownKeys = true
-        }.decodeFromString<DownloadResponse>(response)
-        file = File("${downloadResponse.metadata.artists} - ${downloadResponse.metadata.title}.mp3")
-        url = downloadResponse.link
-    }*/
-
-    /*override fun download() {
-        val response = request("https://api.spotifydown.com/download/$id")
-
-            .execute()
-            .returnContent()
-            .asString()
-        val downloadResponse = Json {
-            ignoreUnknownKeys = true
-        }.decodeFromString<DownloadResponse>(response)
-        val file = File("${downloadResponse.metadata.artists} - ${downloadResponse.metadata.title}.mp3")
-        val url = downloadResponse.link
-        logger.info { "Downloading track ${file.path}" }
-        request(url)
-            .execute()
-            .saveContent(file)
-    }*/
     override fun download() {
         val downloadResponse = restClientBuilder
             .build()
