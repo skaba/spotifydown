21a39f572fd01c691acf6acb35826fef6f716cc8
Serkan Kaba <serkan_kaba@yahoo.com>
2024-06-19 19:05:39 +0300

    Add CompositeResolverTest

--- src/main/kotlin/com/serkank/spotifydown/service/resolver/CompositeResolver.kt
+++ src/main/kotlin/com/serkank/spotifydown/service/resolver/CompositeResolver.kt
@@ -1,12 +1,16 @@
 package com.serkank.spotifydown.service.resolver
 
 import com.serkank.spotifydown.model.Track
+import com.serkank.spotifydown.model.Type
 import com.serkank.spotifydown.model.Url
 import org.springframework.stereotype.Service
 
 @Service
-class CompositeResolver(private val resolvers: List<AppearsInFile>) {
+class CompositeResolver(resolvers: List<AppearsInFile>) {
+
+    private final val resolvers: Map<Type, AppearsInFile> = resolvers.associateBy(AppearsInFile::getType)
+
     fun resolveTracks(url: Url): Sequence<Track> {
-        return resolvers.find { it.getType() == url.type }!!.resolveTracks(url.id)
+        return resolvers[url.type]!!.resolveTracks(url.id)
     }
 }
---
+++ src/test/kotlin/com/serkank/spotifydown/service/resolver/CompositeResolverTest.kt
@@ -0,0 +1,56 @@
+package com.serkank.spotifydown.service.resolver
+
+import com.serkank.spotifydown.model.Track
+import com.serkank.spotifydown.model.Type.*
+import com.serkank.spotifydown.model.Url
+import org.junit.jupiter.api.assertThrows
+import org.mockito.Mockito.`when`
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.mock.mockito.SpyBean
+import kotlin.test.Test
+import kotlin.test.assertFailsWith
+import kotlin.test.assertSame
+
+@SpringBootTest
+class CompositeResolverTest {
+
+    val id: String = "ID"
+    val returnValue = emptySequence<Track>()
+
+    @SpyBean
+    lateinit var playlistResolver: PlaylistResolver
+
+    @SpyBean
+    lateinit var albumResolver: AlbumResolver
+
+    @SpyBean
+    lateinit var trackResolver: TrackResolver
+
+    @Autowired
+    lateinit var compositeResolver: CompositeResolver
+
+    @Test
+    fun testPlaylistResolver() {
+        `when`(playlistResolver.resolveTracks(id)).thenReturn(returnValue)
+        assertSame(returnValue, compositeResolver.resolveTracks(Url(PLAYLIST, id)))
+    }
+
+
+    @Test
+    fun testAlbumResolver() {
+        `when`(albumResolver.resolveTracks(id)).thenReturn(returnValue)
+        assertSame(returnValue, compositeResolver.resolveTracks(Url(ALBUM, id)))
+    }
+
+    @Test
+    fun testTrackResolver() {
+        `when`(trackResolver.resolveTracks(id)).thenReturn(returnValue)
+        assertSame(returnValue, compositeResolver.resolveTracks(Url(TRACK, id)))
+    }
+
+    @Test
+    fun testFileResolver() {
+        assertFailsWith<NullPointerException> { compositeResolver.resolveTracks(Url(FILE, id)) }
+    }
+}
