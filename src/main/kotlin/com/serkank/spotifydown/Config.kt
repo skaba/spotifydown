dda7782574fcac048107913eaa22179cad760a6f
Serkan Kaba <serkan_kaba@yahoo.com>
2024-10-15 21:51:12 +0200

    Don't make session a bean.

--- src/main/kotlin/com/serkank/spotifydown/Config.kt
+++ src/main/kotlin/com/serkank/spotifydown/Config.kt
@@ -9,8 +9,6 @@ import org.springframework.shell.command.annotation.CommandScan
 import org.springframework.web.client.RestClient
 import org.springframework.web.client.support.RestClientAdapter
 import org.springframework.web.service.invoker.HttpServiceProxyFactory
-import xyz.gianlu.librespot.core.Session
-import java.io.File
 
 @Configuration
 @CommandScan
@@ -40,34 +38,5 @@ class Config {
                 .build()
         return factory
             .createClient(SpotifyDownService::class.java)
-    }
-
-    @Bean
-    fun session(): Session {
-        // val credentialsFile =
-        // credentialsFile.createNewFile()
-        val conf =
-            Session.Configuration
-                .Builder()
-                .setStoreCredentials(true)
-                .setStoredCredentialsFile(File(System.getProperty("user.home"), ".spotify_down"))
-                .setCacheEnabled(false)
-                /*.setStoreCredentials(true)
-                .setStoredCredentialsFile()
-                .setTimeSynchronizationMethod()
-                .setTimeManualCorrection()
-                .setProxyEnabled()
-                .setProxyType()
-                .setProxyAddress()
-                .setProxyPort()
-                .setProxyAuth()
-                .setProxyUsername()
-                .setProxyPassword()
-                .setRetryOnChunkError()                 */
-                .build()
-        return Session
-            .Builder(conf)
-            .oauth()
-            .create()
     }
 }
