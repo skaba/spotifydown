ee7fb47ba8a42febfcb832751a9c32a30e85a0bf
Serkan Kaba <serkan_kaba@yahoo.com>
2025-05-19 09:38:16 +0200

    Serialization is not used anymore.

--- src/main/kotlin/com/serkank/spotifydown/Config.kt
+++ src/main/kotlin/com/serkank/spotifydown/Config.kt
@@ -1,10 +1,7 @@
 package com.serkank.spotifydown
 
-import kotlinx.serialization.json.Json
-import org.springframework.boot.web.codec.CodecCustomizer
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.http.codec.json.KotlinSerializationJsonDecoder
 import org.springframework.shell.command.annotation.CommandScan
 import xyz.gianlu.librespot.core.Session
 import java.io.File
@@ -12,18 +9,6 @@ import java.io.File
 @Configuration
 @CommandScan
 class Config {
-    @Bean
-    fun codecCustomizer(): CodecCustomizer =
-        CodecCustomizer {
-            it.defaultCodecs().kotlinSerializationJsonDecoder(
-                KotlinSerializationJsonDecoder(
-                    Json {
-                        ignoreUnknownKeys = true
-                    },
-                ),
-            )
-        }
-
     @Bean(destroyMethod = "close")
     fun session(): Session {
         // val credentialsFile =
