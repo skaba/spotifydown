096bcd1e5c69d8661e94c0f4ef864e829e8a8f52
Serkan Kaba <serkan_kaba@yahoo.com>
2026-02-09 13:40:53 +0100

    Tag MP3

--- src/main/kotlin/com/serkank/spotifydown/service/TrackDownloaderService.kt
+++ src/main/kotlin/com/serkank/spotifydown/service/TrackDownloaderService.kt
@@ -6,11 +6,13 @@ import com.serkank.spotifydown.model.Track
 import com.spotify.metadata.Metadata
 import io.github.oshai.kotlinlogging.KotlinLogging
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.IO
 import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.runBlocking
 import org.apache.commons.io.IOUtils
+import org.jaudiotagger.audio.mp3.MP3File
+import org.jaudiotagger.tag.FieldKey
+import org.jaudiotagger.tag.id3.ID3v24Tag
 import org.springframework.stereotype.Service
 import vavi.sound.sampled.mp3.MpegAudioFileWriter
 import xyz.gianlu.librespot.core.Session
@@ -46,7 +48,8 @@ class TrackDownloaderService(
         track: Track,
         dryRun: Boolean,
     ): Boolean {
-        val filename = getFilename(track) ?: return false
+        val metaData = getMetaData(track) ?: return false
+        val filename = metaData.filename
         val file = File(filename)
         if (file.exists()) {
             logger.info { "$file already downloaded, skipping" }
@@ -85,26 +88,40 @@ class TrackDownloaderService(
             val mp3InputStream = AudioSystem.getAudioInputStream(mp3AudioFormat, pcmInputStream)
             AudioSystem.write(mp3InputStream, MpegAudioFileWriter.MP3, output)
             IOUtils.closeQuietly(output, mp3InputStream, pcmInputStream, input)
+            tagFile(file, metaData)
             logger.info { "Downloaded $filename" }
             return true
         } catch (e: Exception) {
             logger.debug(e) { "Error downloading ${track.url}" }
-            logger.error { "Error downloading $filename" }
+            logger.error { "Error downloading $filename: ${e.message}" }
             logMissing(track)
             return false
         }
     }
 
-    private fun getFilename(track: Track): String? {
+    private fun tagFile(
+        mp3: File,
+        metadata: Metadata.Track,
+    ) {
+        val mP3File = MP3File(mp3)
+        val tag = ID3v24Tag()
+        tag.setField(FieldKey.ARTIST, metadata.artistList[0].name)
+        tag.setField(FieldKey.ALBUM_ARTISTS, *metadata.artistList.map { it.name }.toTypedArray())
+        tag.setField(FieldKey.TITLE, metadata.name)
+        tag.setField(FieldKey.ALBUM, metadata.album.name)
+        mP3File.iD3v2Tag = tag
+        mP3File.save()
+    }
+
+    private fun getMetaData(track: Track): Metadata.Track? {
         val trackId = TrackId.fromBase62(track.id)
         try {
             return session
                 .api()
                 .getMetadata4Track(trackId)
-                .let { it.filename }
         } catch (e: Exception) {
-            logger.debug(e) { "Error downloading ${track.url}" }
-            logger.error { "Error downloading ${track.url}" }
+            logger.debug(e) { "Error getting metadata for ${track.url}" }
+            logger.error { "Error getting metadata for ${track.url}: ${e.message}" }
             logMissing(track)
             return null
         }
